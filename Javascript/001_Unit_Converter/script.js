3d3cef7a6f220364d4df77fe356a5bbd95da3c22
Allen <Allenk1083>
2024-04-05 16:26:28 -0400

    Made HTML display Javascript

--- Javascript/001_Unit_Converter/script.js
+++ Javascript/001_Unit_Converter/script.js
@@ -1,7 +1,12 @@
 let inputValue = 10;
 let milesToKm = true;
 result = 0;
+
 if(milesToKm === true){result = inputValue * 1.60934}
 else {result = inputValue / 1.60934}
+
 const resultString = inputValue + " miles are " + result + " km";
 console.log(resultString)
+
+let resultElement = document.getElementById('resultElement')
+resultElement.innerHTML = resultString;
