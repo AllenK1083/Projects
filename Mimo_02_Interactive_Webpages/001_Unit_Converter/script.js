0a7a68cfa164077284e9d9de8f618a12e1b0a9f0
Allen <AllenK1083@gmail.com>
2024-05-22 13:39:01 -0400

    Unit Converter added Celsisus and Fahrenheit

--- Mimo_02_Interactive_Webpages/001_Unit_Converter/script.js
+++ Mimo_02_Interactive_Webpages/001_Unit_Converter/script.js
@@ -1,12 +1,16 @@
-let inputValue = 10;
-let milesToKm = true;
-result = 0;
-
-if(milesToKm === true){result = inputValue * 1.60934}
-else {result = inputValue / 1.60934}
-
-const resultString = inputValue + " miles are " + result + " km";
-console.log(resultString)
-
-let resultElement = document.getElementById('resultElement')
-resultElement.innerHTML = resultString;
+function convert() {
+    const inputValue = document.getElementById("userInput").value;
+    const unit = document.getElementById('unit').value;
+    
+    let result = 0;
+    if (unit === "milesToKm") {
+      result = inputValue * 1.60934;
+    } else if (unit === "kmToMiles"){
+      result = inputValue / 1.60934;
+    } else if (unit === "FahrenheitToCelsius"){
+      result = (inputValue - 32) * 5 / 9;
+    } else if (unit === "CelsiusToFahrenheit"){
+      result = (inputValue * 1.8) + 32
+    }
+    document.getElementById('resultElement').innerHTML = result;
+  }
