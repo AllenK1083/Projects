7b8908d2e938d16a18baa3fbe227e7233be6750e
Allen <AllenK1083@gmail.com>
2024-05-28 15:02:56 -0400

    004 Class Test Results JS added variables

---
+++ Mimo_02_Interactive_Webpages/004_Class_Test _Results/script.js
@@ -0,0 +1,12 @@
+let maxPoints = 120;
+let passingPoints = 70;
+let samPoints = 84;
+let alexPoints = 45;
+let jessiePoints = 99;
+let didAlexPass = false;
+
+const listItemArray = document.querySelectorAll("li");
+listItemArray[0].innerHTML = "Raw Points:";
+listItemArray[4].innerHTML = "Percentages:";
+listItemArray[8].innerHTML = "Missed Points:";
+listItemArray[12].innerHTML = "Final Notes:";
