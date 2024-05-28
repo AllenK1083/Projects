d81d34968fda894b69b281b5c5e4f64e96a58631
Allen <AllenK1083@gmail.com>
2024-05-28 16:23:35 -0400

    Calculate time, tankrange, leftover gas

--- Mimo_02_Interactive_Webpages/005_Trip_Planner/script.js
+++ Mimo_02_Interactive_Webpages/005_Trip_Planner/script.js
@@ -2,3 +2,10 @@ let averageSpeed =  57;
 let mpg = 27;
 let gallons = 13; 
 let destination = "Ithaca, NY";
+let tripMiles = 74+63+167;
+let time = tripMiles/averageSpeed;
+let tankRange = mpg*gallons;
+let leftoverGasRange = tankRange -  tripMiles;
+console.log("Trip to " + destination)
+console.log("Estimated Time: "+time)
+console.log("Range Remaining: "+leftoverGasRange)
