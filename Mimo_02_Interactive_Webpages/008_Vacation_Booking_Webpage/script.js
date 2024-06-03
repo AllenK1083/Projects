92e7c2cd34903aa9bfe180fd07cea221d03de342
sonic <Allenk1083@gmail.com>
2024-06-03 13:36:55 -0400

    008 Vacation Booking added message that will update on html

--- Mimo_02_Interactive_Webpages/008_Vacation_Booking_Webpage/script.js
+++ Mimo_02_Interactive_Webpages/008_Vacation_Booking_Webpage/script.js
@@ -1,6 +1,6 @@
 const beds = 6;
 const reviewScore = 4.78;
-const costPerNight = 150;
+const costPerNight = 140;
 const pool = true;
 const country = "Italy";
 const minBeds = 5;
@@ -8,7 +8,44 @@ const minReviewScore = 3.5;
 const maxCostPerNight = 150;
 const needPool = true;
 const inCountry = "Italy";
+
 let match = true;
+let saving = "";
+let message = "";
+
+if (beds < minBeds) {
+  match = false;
+};
+
+if (reviewScore < minReviewScore) {
+  match = false;
+};
+
+if (needPool && !pool) {
+  match = false;
+};
+
+if (inCountry !== country){
+  match = false;
+};
+
+if (costPerNight > maxCostPerNight){
+  match = false;
+} else {
+  saving = maxCostPerNight - costPerNight;
+};
+
+if (match){
+  message = "This property is a match!";
+  if (saving){
+    message = message + " It is $" + saving + " cheaper than your maximum.";
+  }
+};
+
+if (match){
+  document.getElementById('message').innerText = message;
+} else {document.getElementById('message').innerText = "Sorry, this property is not suitable."};
+
 const listItemArray = document.querySelectorAll("li");
 listItemArray[0].innerHTML = "Beds: " + beds;
 listItemArray[1].innerHTML = "Review Score: " + reviewScore;
@@ -20,3 +57,4 @@ listItemArray[6].innerHTML = "Minimum Review Score: " + minReviewScore;
 listItemArray[7].innerHTML = "Maximum Cost: " + maxCostPerNight;
 listItemArray[8].innerHTML = "Must Have Pool: " + ((needPool) ? "Yes" : "No");
 listItemArray[9].innerHTML = "Location: " + inCountry;
+
