c8df0ef7d1ce7e58deb4ab17c91cb95a3d655bab
sonic <Allenk1083@gmail.com>
2024-06-03 13:51:31 -0400

    Movie Tickets Project added conditions for pricing and discounts

--- Mimo_02_Interactive_Webpages/009_Movie_Tickets/script.js
+++ Mimo_02_Interactive_Webpages/009_Movie_Tickets/script.js
@@ -1,3 +1,22 @@
-let member_number = 21543;
+let member_number
 let age = 65;
-let price
+let price ;
+age = 10;
+age = 27;
+
+if (age >= 65){
+  price = 5;
+} else if (age <= 10){
+  price = 8;
+} else {
+  price = 10;
+}
+
+const isMember = (member_number > 0);
+
+if (isMember){
+  console.log("Welcome Back!");
+  price = (price*.8);
+}
+
+console.log("Ticket Price: $" + price);
