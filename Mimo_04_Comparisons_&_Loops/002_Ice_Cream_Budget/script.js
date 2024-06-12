4d245317e2f8115b136d351f1c3d9b99243fbb56
sonic <Allenk1083@gmail.com>
2024-06-12 14:35:06 -0400

    ice cream budget included budget and taxes applied

--- Mimo_04_Comparisons_&_Loops/002_Ice_Cream_Budget/script.js
+++ Mimo_04_Comparisons_&_Loops/002_Ice_Cream_Budget/script.js
@@ -1,2 +1,20 @@
 let price = 2.50;
 let campers = 15;
+
+console.log(price);
+console.log(campers);
+
+let taxRate = .08;
+console.log(taxRate);
+
+let budget = 40;
+
+let total = ((price * campers)* taxRate)+(price*campers);
+console.log(total)
+
+let totalExceedsBudget = (total > budget);
+
+console.log("Total Exceeds Budget? " + totalExceedsBudget);
+
+let taxApplied = total > (price*campers);
+console.log("Tax Applied? " + taxApplied);
