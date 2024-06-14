dad7098fe44978e0a3aa2f755723c90c8c5a0c55
Admin <allenk1083@gmail.com>
2024-06-14 12:41:39 -0400

    Started Rent Comparison Project

---
+++ Mimo_04_Comparisons_&_Loops/004_Rent_Comparison/script.js
@@ -0,0 +1,9 @@
+const currentCity = "Paris";
+const compCity = "Barcelona";
+
+
+document.getElementById("city1").innerHTML = currentCity + ": &euro;" +
+  currentRent;
+document.getElementById("city2").innerHTML = compCity + ": &euro;" + compRent;
+// document.getElementById("comparison").innerHTML = comparisonLabel;
+// document.getElementById("difference").innerHTML = differenceLabel;
