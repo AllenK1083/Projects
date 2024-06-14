0a45bd704b0dc99ac2e3ac7515d5049742b49ac1
Admin <allenk1083@gmail.com>
2024-06-14 13:07:07 -0400

    Rent Comparison JS finished

--- Mimo_04_Comparisons_&_Loops/004_Rent_Comparison/script.js
+++ Mimo_04_Comparisons_&_Loops/004_Rent_Comparison/script.js
@@ -1,9 +1,15 @@
 const currentCity = "Paris";
 const compCity = "Barcelona";
-
-
+const currentRent = 1200;
+const compRent = 940;
+const isMoreExpensive = currentRent > compRent;
+const comparisonLabel = currentCity + " is more expensive: " + isMoreExpensive;
+console.log(comparisonLabel);
+const difference = currentRent - compRent;
+const differenceLabel = "Rent difference: &euro" + difference;
+console.log(differenceLabel);
 document.getElementById("city1").innerHTML = currentCity + ": &euro;" +
   currentRent;
 document.getElementById("city2").innerHTML = compCity + ": &euro;" + compRent;
-// document.getElementById("comparison").innerHTML = comparisonLabel;
-// document.getElementById("difference").innerHTML = differenceLabel;
+document.getElementById("comparison").innerHTML = comparisonLabel;
+document.getElementById("difference").innerHTML = differenceLabel;
