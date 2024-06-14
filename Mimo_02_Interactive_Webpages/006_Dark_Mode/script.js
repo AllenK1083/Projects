7dcdc0254ab6b57aed0b7aa6a54be082de3b56cf
Admin <allenk1083@gmail.com>
2024-06-14 10:27:24 -0400

    reuploaded Dark Mode

---
+++ Mimo_02_Interactive_Webpages/006_Dark_Mode/script.js
@@ -0,0 +1,23 @@
+let mode = "light";
+let color = "ghostWhite";
+let image = "/img/light-icon.png";
+let label = "Light Mode";
+mode = "ninja";
+
+if (mode === "dark"){
+  color = "darkSlateBlue";
+  image = "/img/dark-icon.png";
+  label = "Dark Mode";
+  }
+else if (mode === "light") {
+  color = "ghostWhite";
+  image = "/img/light-icon.png";
+  label = "Light Mode";
+}
+else {
+  color = "dimGray";
+  image = "/img/ninja-icon.png";
+  label = "Ninja Mode";
+}
+
+console.log(label)
