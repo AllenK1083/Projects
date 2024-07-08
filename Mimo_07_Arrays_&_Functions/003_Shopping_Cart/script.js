3442b06dbc84cd73986ce38feb38c5e3562bf14c
sonic <Allenk1083@gmail.com>
2024-07-08 13:41:01 -0400

    Shopping Cart Finished

--- Mimo_07_Arrays_&_Functions/003_Shopping_Cart/script.js
+++ Mimo_07_Arrays_&_Functions/003_Shopping_Cart/script.js
@@ -4,3 +4,10 @@ const headphones = 50;
 const mat = 25;
 const wishlist = [book, gardeningTools, headphones, mat];
 const cart = [];
+let item1 = wishlist.pop();
+const item2 = wishlist.pop();
+cart.splice(0,0, item1, item2);
+console.log("Cart prices:");
+console.log(cart);
+const total = item1 + item2;
+console.log(`Total: ${total}`)
