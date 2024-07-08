4716fdb4f45aaeabc6c540f9ed03bbbfa8c4dc4a
sonic <Allenk1083@gmail.com>
2024-07-08 14:52:28 -0400

    Friend Recommendation

--- Mimo_07_Arrays_&_Functions/005_Friend_Recommendation/script.js
+++ Mimo_07_Arrays_&_Functions/005_Friend_Recommendation/script.js
@@ -1,2 +1,9 @@
 const samsFriends = [];
 samsFriends.push("morgan","riley","jessie","skyler","frankie");
+const ashersFriends = [];
+ashersFriends.push("morgan","aiden","angel","ariel","skyler");
+const friendsRecommendationAsher = [];
+let currentFriend = "";
+for (let i = 0; i < samsFriends.length; i++) {
+    currentFriend = samsFriends[i];
+}   
