b92cbd065e450a648550d7be26b4d7b6918713d7
sonic <Allenk1083@gmail.com>
2024-07-08 11:17:54 -0400

    Soccer Team Project Finished

---
+++ Mimo_07_Arrays_&_Functions/002_Soccer_Team/script.js
@@ -0,0 +1,8 @@
+const goalie = ["Sam"];
+console.log (`Goalie: ${goalie}`);
+const defenders = ["Lisa", "Tom", "Jenny", "Jo"];
+console.log (`Defenders: ${defenders}`);
+const midfielders = ["Jim","Kai","Ian","Ava"];
+console.log(`Midfielders: ${midfielders}`);
+const forwards = ["Kim", "Lucas"];
+console.log(`Forwards: ${forwards}`);
