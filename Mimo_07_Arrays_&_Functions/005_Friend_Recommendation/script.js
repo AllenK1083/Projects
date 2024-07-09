51835b491ff1135ee0453d9828433d66382ba786
sonic <Allenk1083@gmail.com>
2024-07-09 12:59:27 -0400

    Finished friends recommendation

--- Mimo_07_Arrays_&_Functions/005_Friend_Recommendation/script.js
+++ Mimo_07_Arrays_&_Functions/005_Friend_Recommendation/script.js
@@ -6,4 +6,10 @@ const friendsRecommendationAsher = [];
 let currentFriend = "";
 for (let i = 0; i < samsFriends.length; i++) {
     currentFriend = samsFriends[i];
-}   
+    let isCommonFriend = false;
+if (!isCommonFriend) {
+    friendsRecommendationAsher.push(currentFriend);
+};}
+   
+console.log("Friends Recommended for Asher:");
+console.log(friendsRecommendationAsher);
