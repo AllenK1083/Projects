d8f4e33384e74ef41f497ed4084d05e1006bd803
Admin <allenk1083@gmail.com>
2024-07-10 14:45:28 -0400

    Finished Text Analyzer

--- Mimo_07_Arrays_&_Functions/009_Text_Analyzer/script.js
+++ Mimo_07_Arrays_&_Functions/009_Text_Analyzer/script.js
@@ -10,8 +10,15 @@ const  textAnalyzer = (text)=> {
       "Cannot analyze text. Your text must include at least one character.");
     return "An error occurred";
   }
-    let shortestWord = '';
-    let longestWord = '';
+    const wordsArray = convertTextToWords(text);
+    let longestWord = findLongestWord(wordsArray);
+    let shortestWord = findShortestWord(wordsArray, longestWord); // Pass both parameters.
+    console.log(`Longest Word: ${longestWord}`);
+    console.log(`Shortest Word: ${shortestWord}`);
+    const vowelWords  = findVowelWords(wordsArray);
+    console.log(`Vowel Words: ${vowelWords}`)
+    console.log(`Word list: ${wordsArray}`);
+    console.log(`Total Words Count: ${wordsArray.length}`)
     return longestWord + shortestWord;
   };
   
@@ -23,3 +30,41 @@ const  textAnalyzer = (text)=> {
     return words
   
   }
+  
+  function findLongestWord(words) {
+    let longestWord = '';
+    for (let i = 0; i < words.length; i++) {
+      let currentWord = words[i];
+      if (currentWord.length >= longestWord.length) {
+        longestWord = currentWord;
+      }
+    }
+    return longestWord;
+  }
+  
+  function findShortestWord(words,longestWord) {
+    let shortestWord = longestWord;
+    for (let i = 0; i < words.length; i++) {
+      let currentWord = words[i];
+      if (currentWord.length < shortestWord.length) {
+        shortestWord = currentWord;
+      }
+    }
+    return shortestWord; // Add this line.
+  }
+  
+  function findVowelWords(words) {
+    const vowels = ['a', 'i', 'e', 'o', 'u'];
+    let vowelWords = [];
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+      for (let j = 0; j < vowels.length; j++) {
+        let vowel = vowels[j];
+        if (word.startsWith(vowel)) {
+          vowelWords.push(word);
+        }
+      }
+    }
+    return vowelWords;
+  }
+  textAnalyzer(`You are an awesome developer`);
