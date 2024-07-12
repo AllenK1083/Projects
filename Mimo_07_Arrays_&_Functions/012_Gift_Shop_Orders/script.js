cd1dbf6c10b17b80d66e74007ab44b17f43e744f
Allen <AllenK1083@gmail.com>
2024-07-12 15:59:24 -0400

    Finished Restaurant and Imported Gift Shop

---
+++ Mimo_07_Arrays_&_Functions/012_Gift_Shop_Orders/script.js
@@ -0,0 +1,7 @@
+let sales = [455, 505, 340, 780, 690, 880, 900, 685, 835, 754];
+let customers = [25, 30, 20, 35, 36, 32, 44, 45, 34, 15];
+let daysOfTheWeek = ["Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"];
+
+
+
+
