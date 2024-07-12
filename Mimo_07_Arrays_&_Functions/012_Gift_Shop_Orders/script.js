7141ae93d9b85d99777134a8cf812361d274ef32
Allen <AllenK1083@gmail.com>
2024-07-12 16:26:35 -0400

    Halway finished

--- Mimo_07_Arrays_&_Functions/012_Gift_Shop_Orders/script.js
+++ Mimo_07_Arrays_&_Functions/012_Gift_Shop_Orders/script.js
@@ -2,12 +2,9 @@ let sales = [455, 505, 340, 780, 690, 880, 900, 685, 835, 754];
 let customers = [25, 30, 20, 35, 36, 32, 44, 45, 34, 15];
 let daysOfTheWeek = ["Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"];
 
-let firstWeekSales = sales.filter(function(element, index) {
- return index < 7;
-});
+let firstWeekSales = sales.filter((element, index) => index < 7);
 console.log("First Week Sales: " + firstWeekSales);
 
-let firstWeekCustomers = customers.filter(function(element, index) { return index < 7;
-});
+let firstWeekCustomers = customers.filter((element, index) => index < 7);
 
 console.log(`First Week Customers: ${firstWeekCustomers}`);
