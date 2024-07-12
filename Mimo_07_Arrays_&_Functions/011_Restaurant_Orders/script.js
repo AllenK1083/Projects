f46a73acdb88a3f25b3ffcd01085804fdb347e59
Allen <AllenK1083@gmail.com>
2024-07-12 15:11:52 -0400

    Restaurant Order Imported

---
+++ Mimo_07_Arrays_&_Functions/011_Restaurant_Orders/script.js
@@ -0,0 +1,33 @@
+const italianFood = [{
+    mealName: "Pasta Bolognese",
+    quantity: 3,
+    price: 7
+  }, {
+    mealName: "Pepperoni pizza",
+    quantity: 2,
+    price: 10
+  }];
+  const thaiFood = [{
+    mealName: "Pad Thai",
+    quantity: 1,
+    price: 8
+  }];
+  const indianFood = [{
+    mealName: "Chapati",
+    quantity: 8,
+    price: 7
+  }, {
+    mealName: "Chicken Massala",
+    quantity: 5,
+    price: 9
+  }, {
+    mealName: "Tandoori Chicken",
+    quantity: 5,
+    price: 9
+  }];
+  
+  function findMeal(name, menu) {}
+  
+  function selectMeal(name, type) {}
+  
+  function createSummary(name, type, amount) {}
