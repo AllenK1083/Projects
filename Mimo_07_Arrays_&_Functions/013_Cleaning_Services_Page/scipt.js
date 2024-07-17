c8a6c62e9204384cae0490f4789049dfbc19167b
Admin <allenk1083@gmail.com>
2024-07-17 12:06:40 -0400

    query selector added

---
+++ Mimo_07_Arrays_&_Functions/013_Cleaning_Services_Page/scipt.js
@@ -0,0 +1,6 @@
+const allButton = document.querySelector("#allButton");
+const insideButton = document.querySelector("#insideButton");
+const outsideButton = document.querySelector("#outsideButton");
+const lawnButton = document.querySelector("#lawnButton");
+const cleaningButton = document.querySelector("#cleaningButton");
+const clearEstimateButton = document.querySelector("#clearEstimateButton");
